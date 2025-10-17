bd31285fb9a3975f368e04811c9d02cb9e3957ec
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-17 16:42:51 -0300

    add: doc

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // <- Evita el uso de rutas super relativas como por ej: ../../components/etc
     },
   },
 });
